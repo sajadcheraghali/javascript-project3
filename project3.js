3fb89cf95a6bdbec0f349cd860612ce0173fa23c
sajadcheraghali <sajad.cheraghalii@gmail.com>
2024-05-26 19:56:30 +0330

    Add files via upload

---
+++ project3.js
@@ -0,0 +1,50 @@
+function addnote() {  //get input
+    let inputnote = document.querySelector('input')
+    let newnote = document.createElement('div')
+    newnote.className='list'
+    newnote.innerHTML = inputnote.value
+    newnote.style.backgroundColor = inputnote.style.backgroundColor
+    let newtrash = document.createElement('img')
+    newtrash.className = 'trash'
+    newtrash.setAttribute('src','https://icongr.am/entypo/trash.svg?size=20&color=currentColor')
+    newtrash.setAttribute('title','remove')
+    newnote.append(newtrash)
+    let mainlist =  document.getElementById('mainlist')
+    mainlist.appendChild(newnote)
+    inputnote.value=''
+    inputnote.style.backgroundColor='aqua'
+}
+//  change colore of input background
+function red(){
+    document.querySelector('input').style.backgroundColor = 'red'
+}
+function blue(){
+    document.querySelector('input').style.backgroundColor = 'blue'
+}
+function green(){
+    document.querySelector('input').style.backgroundColor = 'green'
+}
+function white(){
+    document.querySelector('input').style.backgroundColor = 'white'
+}
+//  remove note from list
+window.onload = function(){
+    let mainlist =  document.getElementById('mainlist')
+    mainlist.addEventListener('click',function(event){
+    console.log(event)
+    let trash1 = document.querySelectorAll('.trash')
+    console.log(trash1)
+    console.log(event.target)
+
+    for(let i=0;i<trash1.length;i++){
+         if(trash1[i] == event.target){
+            trash1[i].parentElement.remove()
+         }
+        }
+    })
+}
+// erease data in input
+function inputtrash(){
+    let inputnote = document.querySelector('input')
+    inputnote.value=''
+}
